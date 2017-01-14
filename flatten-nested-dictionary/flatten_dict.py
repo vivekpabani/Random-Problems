c2b47fa9411b0e1ea9a4565a5dc8ee5797b4da09
Vivek Pabani <vpabani@hawk.iit.edu>
2017-01-14 01:25:46 -0600

    implemented iterative flatten.

--- flatten-nested-dictionary/flatten_dict.py
+++ flatten-nested-dictionary/flatten_dict.py
@@ -61,6 +61,41 @@ def flatten_dict_rec(in_dict, prefix=''):
     return out_dict 
 
 
+def flatten_dict_iter(in_dict, prefix=''):
+    """
+    Given a dictionary, flatten it iteratively.
+    :param in_dict (dict): input dictionary
+    :param prefix (str) [optional] [default=' ']: the prefix for keys of current dictionary.
+
+    :return (dict): flatten dictionary.
+    """
+
+    out_dict = dict()
+    stack = list()
+
+    stack.append([in_dict, prefix])
+
+    while stack:
+        top_item = stack.pop()
+        curr_dict, prefix = top_item[0], top_item[1]
+
+        for key, value in curr_dict.items():
+
+            if isinstance(value, dict):
+                # if current key value pair is a nested dictionary,
+                # add the current value (dict), and updated prefix pair to the stack.
+
+                stack.append([value, prefix + key + '.'])
+
+            else:
+                # if current value is a string/normal value,
+                # add it to the output dictionary directly with proper key.
+
+                out_dict[prefix + key] = value
+
+    return out_dict
+
+
 def main():
 
     in_dict = {
@@ -79,6 +114,10 @@ def main():
 
     print("\nRecursive flatten: ")
     print(flatten_dict_rec(in_dict))
+
+    print("\nIterative flatten: ")
+    print(flatten_dict_iter(in_dict))
+
 
 if __name__ == "__main__":
     main()
